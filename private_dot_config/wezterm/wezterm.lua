928bdedb96b86a6aa8f57a2a7404c7766be12cf4
Jonathan Puide <jonte.puide@gmail.com>
2025-08-10 08:10:06 +0200

    Trying to fix font problems on alpine

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually specify your configuration options
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
-config.enable_wayland = true
+config.enable_wayland = false
 config.window_background_opacity = 0.75
 -- and finally, return the configuration to wezterm
 return config
