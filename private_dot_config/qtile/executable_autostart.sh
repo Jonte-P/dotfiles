e17dd7257ffda51c06719e0ade2c5308047f9b9a
Jonathan Puide <jonte.puide@gmail.com>
2025-12-30 08:36:02 +0100

    Updated autostart file

--- private_dot_config/qtile/executable_autostart.sh
+++ private_dot_config/qtile/executable_autostart.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-
-pipewire &
+run() {
+  if ! pgrep -f "$1" ;
+  then
+    "$@"&
+  fi
+}
+run "/usr/bin/pipewire"
+run "/usr/libexec/xfce-polkit"
+run "xfce4-power-manager"
 xset b off
 nitrogen --restore
-compton &
+run "compton"
